287880e682c617e76edc14ee4407a27ed9de1fe4
RandyRomero <a.ya.miheev@gmail.com>
2018-12-08 14:13:40 +0300

    Push all the files to the github

---
+++ html_converter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*- 
+
+from aiohttp import web
+
+async def generate(request):
+	pass
+
+
+async def hello(request):
+	name = request.match_info.get('name', 'Anonymous')
+	text = "Hello, " + name
+	return web.Response(text=text)
+
+
+def main():
+	app = web.Application()
+	app.router.add_get('/hello/{name}', hello)
+	app.router.add_post('/generate', generate)
+
+	web.run_app(app)
+
+if __name__ == '__main__':
+	main()
