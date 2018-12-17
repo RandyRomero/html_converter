166a065255e129ec9e7aa72b1bd383450e208a52
RandyRomero <a.ya.miheev@gmail.com>
2018-12-17 20:22:34 +0300

    Finally the app can communicate with the athenapdf microservice, i.e. get an html-file from a client, send it to athenapdf to convert, get it back and send it back to user

--- hc_server.py
+++ hc_server.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*- 
 
-import logging
+import aiohttp
 from aiohttp import web
 import hashlib
 from set_up_logging import get_logger
 
-logger = get_logger(__name__)
 
-async def make_request(session, url):
-    async with session.get(url) as response:
-        print('wtf')
-        return await response
+logger = get_logger(__name__)
 
 
 async def get_pdf(md5):
-    logger.debug('Trying to connect to arachnysdocker...')
-    # async with aiohttp.ClientSession() as session:
-    #     url = f'http://0.0.0.0:5432/convert?auth=arachnys-weaver&url=http://0.0.0.0:8080/raw/{md5}/'
-    #     print('Looks like it works')
-    #     some_result = await make_request(session, url)
-    #     return some_result
-    return "That's your response"
+    async with aiohttp.ClientSession() as session:
+        # url = f'http://172.17.0.2:8080/convert?auth=arachnys-weaver&url=http://127.0.0.1:8181/raw/{md5}'
+        url = f'http://127.0.0.1:8080/convert?auth=arachnys-weaver&url=http://127.0.0.1:8181/raw/{md5}'
+        # url = 'http://127.0.0.1:8080/convert?auth=arachnys-weaver&url=https://blog.arachnys.com/1/google-isnt-even-close-to-proper-due-diligence.-why-not?utm_campaign=athena&utm_medium=external%20website&utm_source=github&utm_content=readme'
+        # url = 'http://127.0.0.1:8080/convert?auth=arachnys-weaver&url=http://docs.aiohttp.org/en/stable/client_advanced.html'
+        logger.debug('Trying to connect to arachnysdocker with url: %s', url)
+
+        async with session.get(url) as response:
+            if response.status == 200:
+                logger.debug('Got the pdf file')
+                raw_pdf = await response.content.read()
+                return raw_pdf
+
+            # todo handle a situation when status is not 200
 
 
 async def get_raw_html(request):
@@ -33,7 +36,7 @@ async def get_raw_html(request):
         return web.Response(text='There is no data for this key', status=404)
 
     logger.debug('Returning queried html as a string...')
-    return web.Response(text=html)
+    return web.Response(body=html, content_type='text/html')
 
 
 async def generate(request):
@@ -47,10 +50,14 @@ async def generate(request):
     logger.debug('Saving the html in app by its md5...')
     request.app['htmls'][md5] = text
 
-    pdf = await get_pdf(md5)
+    pdf_raw_file = await get_pdf(md5)
+
+    del request.app['htmls'][md5]
+    assert request.app['htmls'].get(md5, None) == None, "The html string haven't been removed!"
 
     logger.debug('Returning pdf to the user...')
-    return web.Response(body=pdf, content_type='application/pdf')
+    response = web.Response(body=pdf_raw_file, status=200, content_type='application/pdf')
+    return response
 
 
 def main():
@@ -60,8 +67,10 @@ def main():
     app.router.add_get('/raw/{md5}', get_raw_html)
     app.router.add_post('/generate', generate)
 
-    web.run_app(app)
+    web.run_app(app, host='127.0.0.1', port='8181')
 
 
 if __name__ == '__main__':
     main()
+
+# https://stackoverflow.com/questions/24319662/from-inside-of-a-docker-container-how-do-i-connect-to-the-localhost-of-the-mach
--- tests/hc_client.py
+++ tests/hc_client.py
@@ -2,30 +2,43 @@
 
 import aiohttp
 import asyncio
+import aiofiles
 
 
 async def get_md5(session, url):
-	async with session.get(url) as response:
-		return await response.text()
+    async with session.get(url) as response:
+        return await response.text()
 
 
-async def send_html(session, url, payload):
-	async with session.post(url, data=payload) as response:
-		return await response.text()
+async def convert_html_to_pdf(session, url, data):
+    async with session.post(url, data=data) as response:
+        print(response.status)
+        if response.status == 200:
+            async with aiofiles.open('file.pdf', 'wb') as file:
+                while True:
+                    chunk = await response.content.read(1024)
+                    if not chunk:
+                        break
+                    await file.write(chunk)
+        return await response.release()
 
 
 async def main():
-	async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession() as session:
+        # data = 'https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04'
 
-		# test that server gets html
-		payload	 = ['<h1>I am your HTML</h1>', '<p>There is some text</p>', '<a href="www.example.com">click</a>']
-		for p in payload:
-			html = await send_html(session, 'http://localhost:8080/generate', p)
-			print(html)
+        url = 'http://docs.aiohttp.org/en/stable/client_advanced.html'
+        async with session.get(url) as response:
+            if response.status == 200:
+                file = await response.read()
+                # headers = {'content-type': 'application/html'}
+                await convert_html_to_pdf(session, 'http://localhost:8181/generate', data=file)
+            else:
+                print(f'Server returned {response.status} status code')
 
-		# test that server can return html by md5 key
-		html_by_md5 = await get_md5(session, 'http://localhost:8080/raw/b9b876fed08883de8203d1076f8db0bd')
-		print(html_by_md5)
+
+        # test that server can return html by md5 key
+        # response = await get_md5(session, 'http://localhost:8181/raw/44c8ce76fb92401953f0b122182929ab')
 
 
 loop = asyncio.get_event_loop()
